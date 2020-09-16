c5efe26a1f14233ed5fb2d905b968e387cf5f635
Ziheng Zhou <zzmaczz@Zihengs-MacBook-Pro.local>
2020-09-16 13:54:49 +0800

    tidy up

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
-  <div id="app">
+  <div class="m-1" id="app">
     <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" /> -->
-    <div v-if="!isTestNet">VeChainThor TestNet Required</div>
+    <h1 v-if="!isTestNet">VeChainThor TestNet Required</h1>
+    <h1 v-if="isTestNet">ZKVote APP</h1>
     <NewVote v-if="isTestNet"/>
+    <Cast v-if="isTestNet"/>
+    <ListBallots v-if="isTestNet"/>
     <ListVotes v-if="isTestNet"/>
     <!-- <AuthorityPanel v-if="isTestNet" /> -->
   </div>
@@ -12,18 +15,20 @@
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 // import HelloWorld from "./components/HelloWorld.vue";
-import ListVotes from "./components/ListVotes.vue";
 // import AuthorityPanel from "./components/AuthorityPanel.vue";
 import NewVote from "./components/NewVote.vue"
+import Cast from "@/components/Cast.vue"
+import ListVotes from "./components/ListVotes.vue";
+import ListBallots from "@/components/ListBallots.vue"
 
 import "@vechain/connex";
 
 @Component({
   components: {
-    // HelloWorld,
     ListVotes,
-    // AuthorityPanel,
+    ListBallots,
     NewVote,
+    Cast,
   },
 })
 export default class App extends Vue {
